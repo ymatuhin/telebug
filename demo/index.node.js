93aad73cf7b44be59c19fa5fa0f0148498e94d58
ymatuhin <ym@ymatuhin.ru>
2018-01-12 10:41:12 +0300

    1.0.12

--- demo/index.node.js
+++ demo/index.node.js
@@ -1,12 +1,11 @@
-const telebug = require('../dist/index.js');
+const telebug = require('../lib/telebug.js');
 
 const config = {
   botId: '474186924:AAGtoPx1A_q9MoLdhRCin5EmGwN7xlC_21g',
   chatId: '@telebug_development',
 };
 
-const bugInstance = telebug(config);
-bugInstance.addCustomMessage(`From demo node.js`);
+telebug(config);
 
 setTimeout(() => qwerty());
 setTimeout(() => Promise.reject(new Error('woops')));
